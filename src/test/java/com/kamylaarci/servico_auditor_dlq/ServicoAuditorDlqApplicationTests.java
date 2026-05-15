c3d6351c6f485e4956b82cdcb54b4d7cb3f6bc34
Kamyla Arci <kamylaarci@gmail.com>
2026-05-15 09:04:47 -0300

    init: cria estrutura inicial do projeto Spring Boot com Maven

---
+++ src/test/java/com/kamylaarci/servico_auditor_dlq/ServicoAuditorDlqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamylaarci.servico_auditor_dlq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoAuditorDlqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
